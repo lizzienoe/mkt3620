4bc17793da8ba499685c004862910e0a16d859d9
lizzienoe <enoe@iastate.edu>
2026-09-03 13:38:35 -0500

    Add navigation bar to nav.js

---
+++ nav.js
@@ -0,0 +1,15 @@
+const navigation = `
+  <nav class="navbar">
+    <a href="index.html" class="logo">Lizzie Noe</a>
+
+    <div class="nav-links">
+      <a href="#about">About Me</a>
+      <a href="#school">School Life</a>
+      <a href="#experience">Work Experience</a>
+      <a href="#portfolio">Portfolio</a>
+      <a href="#contact">Contact</a>
+    </div>
+  </nav>
+`;
+
+document.getElementById("navigation").innerHTML = navigation;
