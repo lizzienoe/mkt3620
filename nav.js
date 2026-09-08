dfe87e082758022bcf5954195b5a942b5ea2ce13
lizzienoe <enoe@iastate.edu>
2026-09-08 13:20:05 -0500

    Rename navHTML to navigationHTML in nav.js

--- nav.js
+++ nav.js
@@ -1,51 +1,19 @@
-const navHTML = `
+const navigationHTML = `
   <nav class="primary-nav">
-
-    <a href="index.html">
-      Home
-    </a>
-
-    <a href="about.html">
-      About Me
-    </a>
-
-    <a href="school.html">
-      School Life
-    </a>
-
-    <a href="experience.html">
-      Work Experience
-    </a>
-
-    <a href="portfolio.html">
-      Portfolio
-    </a>
-
-    <a href="contact.html">
-      Contact
-    </a>
-
+    <a href="index.html">Home</a>
+    <a href="about.html">About Me</a>
+    <a href="school.html">School Life</a>
+    <a href="experience.html">Work Experience</a>
+    <a href="portfolio.html">Portfolio</a>
+    <a href="contact.html">Contact</a>
   </nav>
 
   <nav class="secondary-nav">
-
-    <a href="about.html#education">
-      Education
-    </a>
-
-    <a href="school.html#involvement">
-      Involvement
-    </a>
-
-    <a href="experience.html#internships">
-      Internships
-    </a>
-
-    <a href="portfolio.html#events">
-      Event Work
-    </a>
-
+    <a href="resume.html">Resume</a>
+    <a href="portfolio.html#events">Events</a>
+    <a href="portfolio.html#marketing">Marketing</a>
+    <a href="school.html#involvement">Involvement</a>
   </nav>
 `;
 
-document.getElementById("navigation").innerHTML = navHTML;
+document.getElementById("navigation").innerHTML = navigationHTML;
