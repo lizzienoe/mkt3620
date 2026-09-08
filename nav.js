bfa4c387f9d22eac07de9bfb45bc153d9df3b7f2
lizzienoe <enoe@iastate.edu>
2026-09-08 13:37:48 -0500

    Revise navigation links in nav.js
    Updated navigation links to include 'About Me', 'School Life', and 'Work Experience'. Removed 'My Goals' link.

--- nav.js
+++ nav.js
@@ -1,15 +1,17 @@
 const navigation = `
   <nav class="primary-nav">
 
-    <a href="index.html">Home</a>
+    <a href="index.html#about">
+      About Me
+    </a>
 
-    <a href="index.html#about">About Me</a>
+    <a href="index.html#school">
+      School Life
+    </a>
 
-    <a href="index.html#school">School Life</a>
-
-    <a href="index.html#experience">Work Experience</a>
-
-    <a href="index.html#goals">My Goals</a>
+    <a href="index.html#experience">
+      Work Experience
+    </a>
 
   </nav>
 `;
